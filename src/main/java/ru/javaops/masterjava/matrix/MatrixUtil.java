13d951185747c797aff75c6b5a20dd69920145f7
FreakPunchess <aleksandr-pisklov@mail.ru>
2023-06-08 21:54:01 +0300

    Init commit

---
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -0,0 +1,67 @@
+package ru.javaops.masterjava.matrix;
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * gkislin
+ * 03.07.2016
+ */
+public class MatrixUtil {
+
+    // TODO implement parallel multiplication matrixA*matrixB
+    public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        return matrixC;
+    }
+
+    // Optimized by https://habrahabr.ru/post/114797/
+    public static int[][] singleThreadMultiplyOpt(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        for (int col = 0; col < matrixSize; col++) {
+            final int[] columnB = new int[matrixSize];
+            for (int k = 0; k < matrixSize; k++) {
+                columnB[k] = matrixB[k][col];
+            }
+
+            for (int row = 0; row < matrixSize; row++) {
+                int sum = 0;
+                final int[] rowA = matrixA[row];
+                for (int k = 0; k < matrixSize; k++) {
+                    sum += rowA[k] * columnB[k];
+                }
+                matrixC[row][col] = sum;
+            }
+        }
+        return matrixC;
+    }
+
+    public static int[][] create(int size) {
+        int[][] matrix = new int[size][size];
+        Random rn = new Random();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = rn.nextInt(10);
+            }
+        }
+        return matrix;
+    }
+
+    public static boolean compare(int[][] matrixA, int[][] matrixB) {
+        final int matrixSize = matrixA.length;
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                if (matrixA[i][j] != matrixB[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
